2b3915b02e3eb4dbf30aea5a453b1d4413721bb5
christian <christianbiringanine22@gmail.com>
2023-07-28 14:57:05 +0200

    set up UI

--- src/components/sign_up_form.jsx
+++ src/components/sign_up_form.jsx
@@ -1,6 +1,18 @@
+import Profil from "../assets/logIn_picture.jpg"
+import "../styles/sign_up.css"
 const SignUpForm = () => {
   return (
-      <h1>SignUpForm</h1>
+      <div className="login_container">
+        <img src={Profil} alt="" />
+        <div className="form__container">
+          <form action="" className="login__form">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name"/>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password"/>
+          </form>
+        </div>
+      </div>
   );
 }
  

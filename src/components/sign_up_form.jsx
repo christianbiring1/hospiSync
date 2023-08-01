d851cb785258cfb0908d3c060c51fbbc4616b0d7
christian <christianbiringanine22@gmail.com>
2023-08-01 14:35:40 +0200

    Reset styles

--- src/components/sign_up_form.jsx
+++ src/components/sign_up_form.jsx
@@ -1,15 +1,30 @@
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import Profil from "../assets/logIn_picture.jpg"
 import "../styles/sign_up.css"
 const SignUpForm = () => {
+
+  const styles = {
+    cursor: "help",
+    fontSize: "18px",
+    color: "gray"
+  }
   return (
       <div className="login_container">
         <img src={Profil} alt="" />
         <div className="form__container">
           <form action="" className="login__form">
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name"/>
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password"/>
+            <div className="input__container">
+              <label htmlFor="name">Name</label>
+              <HelpOutlineIcon style={styles} titleAccess='This is your name'/>
+              <input type="text" id="name"/>
+            </div>
+            <div className="input__container">
+              <label htmlFor="password">Password</label><HelpOutlineIcon style={styles} titleAccess='Your Password'/>
+              <input type="password" id="password"/>
+            </div>
+            <div className="button__container">
+              <button type="submit" className='submit_btn'>CONNEXION</button>
+            </div>
           </form>
         </div>
       </div>

319e3a98705e2cbfc2240e106398942edb59ba5d
christian <christianbiringanine22@gmail.com>
2023-08-03 12:44:42 +0200

    Integrate materialUi

--- src/components/sign_up_form.jsx
+++ src/components/sign_up_form.jsx
@@ -13,18 +13,12 @@ const SignUpForm = () => {
         <img src={Profil} alt="" />
         <div className="form__container">
           <form action="" className="login__form">
-            <div className="input__container">
               <label htmlFor="name">Name</label>
               <HelpOutlineIcon style={styles} titleAccess='This is your name'/>
               <input type="text" id="name"/>
-            </div>
-            <div className="input__container">
               <label htmlFor="password">Password</label><HelpOutlineIcon style={styles} titleAccess='Your Password'/>
               <input type="password" id="password"/>
-            </div>
-            <div className="button__container">
               <button type="submit" className='submit_btn'>CONNEXION</button>
-            </div>
           </form>
         </div>
       </div>
